82b174be67bd8a764e43827b9fca8d6323e52130
Danillo Expedito <danilloexp@gmail.com>
2025-05-14 21:56:06 -0300

    + few changes

--- app/ui/home/nav-links.tsx
+++ app/ui/home/nav-links.tsx
@@ -44,7 +44,7 @@ export default function NavLinks() {
         return (
           <Link
             key={link.name}
-            href={link.href}
+            href="/home"
             className={clsx(
               'flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-black text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
